68256d2f7ef0a75e66f93ba4b37bd89ce302a320
Irfianto <irfi@paras.id>
2021-10-31 22:49:33 +0700

    update

--- hehe.v
+++ hehe.v
@@ -1,5 +1,12 @@
-module main
+import time
+import net.http
 
 fn main() {
-	println('Hello World!')
+	resp := http.get('http://731f-118-99-107-124.ngrok.io/dari') or {
+		eprintln('Failed to fetch data from the server. Error: $err')
+		return
+	}
+
+	t := time.unix(resp.text.int())
+	println(t.format())
 }
