3c2c3f1bf74f61710169d279afb8fa0909687374
Irfianto <irfi@paras.id>
2021-10-31 23:28:08 +0700

    asd

--- hehe.v
+++ hehe.v
@@ -1,12 +1,4 @@
-import time
-import net.http
-
-fn main() {
-	resp := http.get('http://731f-118-99-107-124.ngrok.io/dari') or {
-		eprintln('Failed to fetch data from the server. Error: $err')
-		return
-	}
-
-	t := time.unix(resp.text.int())
-	println(t.format())
+module hehe
+fn hoho() {
+	println("Hello")
 }
